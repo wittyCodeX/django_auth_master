c53d3ce4621fcf61efb6c874e34703dad90fd4c4
maxcore1120 <maxcore1120@gmail.com>
2023-11-07 09:16:02 +0900

    register question model on admin site

--- apps/models.py
+++ apps/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,6 +12,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
