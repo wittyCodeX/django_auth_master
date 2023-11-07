398344b999746853d324e45b13c7002713aadd3e
maxcore1120 <maxcore1120@gmail.com>
2023-11-07 09:47:27 +0900

    add template and change index view

--- apps/views.py
+++ apps/views.py
@@ -7,8 +7,8 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
